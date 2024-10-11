023c78e9a297fa4e567fd4d916271e77fa008ccc
Shariar Duke <shariarmahmud225@gmail.com>
2024-10-11 22:18:32 +0600

    gitignore file uploaded

--- app/main.py
+++ app/main.py
@@ -3,16 +3,21 @@ from . import models, schema , utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 from typing import List
+from dotenv import load_dotenv
+import os
 
 from .routers import post, user , auth
 
-
+load_dotenv()
 
 # Create tables
 models.Base.metadata.create_all(bind=engine)
 
 # Create FastAPI app
 app = FastAPI()
+
+print("Shariar")
+print(int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")))
 
 
 # This section is of user 
